50442a2a50e441da5b5c4792ed04e9e7892d8ec7
Naufal Yudhistira <yudhistn@gmail.com>
2020-04-07 09:47:55 +0700

    Fixing trans service

--- controller/transaksiController.js
+++ controller/transaksiController.js
@@ -3,33 +3,39 @@ const {
   TransaksiDetail,
   Sepatu,
   Keranjang,
-  Retail
+  Retail,
+  User,
 } = require("../models");
 const uuid = require("uuid/v4");
 
 module.exports = {
   index(req, res) {
     if (req.user.role == "Admin") {
-      Transaksi.findAll()
-        .then(data => {
+      Transaksi.findAll({
+        include: {
+          model: User,
+        },
+        order: [["tanggal", "ASC"]],
+      })
+        .then((data) => {
           res.json(data);
         })
-        .catch(err => {
+        .catch((err) => {
           res.json(err);
         });
     } else {
       Transaksi.findAll({
         where: {
-          id_user: req.user.id_user
+          id_user: req.user.id_user,
         },
         include: {
-          model: Retail
-        }
+          model: Retail,
+        },
       })
-        .then(data => {
+        .then((data) => {
           res.json(data);
         })
-        .catch(err => {
+        .catch((err) => {
           res.json(err);
         });
     }
@@ -37,51 +43,62 @@ module.exports = {
   show(req, res) {
     TransaksiDetail.findAll({
       where: {
-        id_transaksi: req.params.id_transaksi
+        id_transaksi: req.params.id_transaksi,
       },
-      include: {
-        model: {
-          Sepatu
-        }
-      }
+      include: [
+        {
+          model: Sepatu,
+        },
+        {
+          model: Transaksi,
+          include: [
+            {
+              model: User,
+            },
+            {
+              model: Retail,
+            },
+          ],
+        },
+      ],
     })
-      .then(data => {
+      .then((data) => {
         res.json(data);
       })
-      .catch(err => {
+      .catch((err) => {
         res.json(err);
       });
   },
   store(req, res) {
     Transaksi.create({
       id_transaksi: uuid(),
-      id_user: req.user.id_user
-    }).then(data => {
+      id_user: req.user.id_user,
+    }).then((data) => {
       Keranjang.findAll({
         where: {
-          id_user: req.user.id_user
+          id_user: req.user.id_user,
         },
         include: {
-          model: Sepatu
-        }
-      }).then(keranjang => {
+          model: Sepatu,
+        },
+      }).then((keranjang) => {
         let grandTotal = 0;
-        keranjang.map(isi => {
+        keranjang.map((isi) => {
           TransaksiDetail.create({
             id_transaksi: data.id_transaksi,
             id_sepatu: isi.id_sepatu,
             jumlah: isi.jumlah,
             harga: isi.Sepatu.harga,
-            subtotal: isi.Sepatu.harga * isi.jumlah
+            subtotal: isi.Sepatu.harga * isi.jumlah,
           });
-          Sepatu.findByPk(isi.id_sepatu).then(spt => {
+          Sepatu.findByPk(isi.id_sepatu).then((spt) => {
             spt.stok -= isi.jumlah;
             spt.save();
           });
-          Transaksi.findByPk(data.id_transaksi).then(row => {
+          Transaksi.findByPk(data.id_transaksi).then((row) => {
             grandTotal += isi.Sepatu.harga * isi.jumlah;
             row.update({
-              total: grandTotal
+              total: grandTotal,
             });
           });
           isi.destroy();
@@ -89,5 +106,5 @@ module.exports = {
       });
       res.json(data);
     });
-  }
+  },
 };
